92f0449a0cdeab7a74825b7086f476cf00d9b26f
Destiny <info.dxmate@gmail.com>
2024-01-09 21:28:20 +0900

    Add Home view

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,12 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { BronzeBadge } from './renderer/components/BronzeBadge';
+import { Home } from './renderer/views/Home';
 
 function App() {
   return (
     <div className="App">
-      <BronzeBadge tier='I' size='small' />
+      <Home />
     </div>
   );
 }
